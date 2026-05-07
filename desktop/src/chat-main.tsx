17bc542be21d50d66911968b08546d4accbd0b1e
jaylfc <jaylfc25@gmail.com>
2026-05-07 13:39:30 +0100

    feat(boot): wrap chat PWA in AppShell

--- desktop/src/chat-main.tsx
+++ desktop/src/chat-main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChatStandalone } from "./ChatStandalone";
+import { AppShell } from "./components/AppShell";
 import "./theme/tokens.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChatStandalone />
+    <AppShell>
+      <ChatStandalone />
+    </AppShell>
   </StrictMode>,
 );
